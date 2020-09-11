83cfbd88dfbb7c592366d8ce4f8ef08d4891a7c8
Adam M. Wilson <adammichaelwilson@gmail.com>
2020-09-11 12:35:28 -0400

    update tests

--- tests.R
+++ tests.R
@@ -4,14 +4,18 @@ library(testthat)
 # Case Study 01
 context("Case Study 01")
 
+# Case study script
+context("Case Study 01 - File Exists")
 f_cs01="../../week_01/case_study_01.R"
 
 test_that(paste0(f_cs01, " file exists"),{
   expect_true(file.exists(f_cs01))
 })
 
+context("Case Study 01 - File runs")
 source(f_cs01)
 
+context("Case Study 01 - Results")
 test_that("petal_length_mean",{
   expect_true(exists("petal_length_mean"))
   expect_equal(petal_length_mean, 3.758)
